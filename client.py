7b9ed5d9a6bcbf87bc62aaeec6e9199472df49df
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2022-04-22 13:42:31 +0000

    update client: add required access tokens

--- client.py
+++ client.py
@@ -11,6 +11,8 @@ load_dotenv()
 BEARER_TOKEN = os.getenv("BEARER_TOKEN")
 API_KEY = os.getenv("API_KEY")
 API_KEY_SECRET = os.getenv("API_KEY_SECRET")
+ACCESS_TOKEN= os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET= os.getenv("ACCESS_TOKEN_SECRET")
 
 
 class Client:
