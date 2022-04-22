1fe9a8af88851cca9a07a217e934970b2e74e1ab
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2022-04-22 13:13:46 +0000

    update client: implement search keyword function

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 import tweepy
+import json
+
 
 # loading environment variables
 load_dotenv()
@@ -17,22 +19,34 @@ class Client:
         Initializes twitter client when an object is created.
         It needs to be an account which we will use for all our requests.
         """
-        __client = tweepy.Client(
+        self.__client = tweepy.Client(
             bearer_token=BEARER_TOKEN,
             consumer_key=API_KEY,
             consumer_secret=API_KEY_SECRET,
             wait_on_rate_limit=True
         )    # creating the client that will be used for the access and requests
 
-    def retrieve_search_keyword_data(self, keyword: str):
+    def retrieve_search_keyword_data(self, keyword: str) -> json:
         """
         retrieves search data about this keyword from the Twitter API
         The 'Essential' access level allows only for retrieving search data from the past week
         :param keyword: the query to search for
-        :return:
+        :return: Response data
         """
-        __new_client = Client()
-        __new_client
+        response = self.__client.search_recent_tweets(
+            query=keyword,
+            max_results=100,
+            expansions=["geo.place_id"],
+            tweet_fields=["context_annotations",    # entity recognition/extraction, topical analysis
+                          "created_at",    # to understand when Tweet was created and used for time-series analysis
+                          "non_public_metrics",   # to determine the total number of impressions generated for the Tweet
+                          "organic_metrics",   # to measure organic engagement for the Tweet
+                          "public_metrics"    # to measure tweet engagement
+                          ],
+            place_fields=["country"]
+        )
+
+        return response.json()
 
     def display_data(self):
         pass
